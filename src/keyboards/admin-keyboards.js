e5c9eb08ae1712a553e025737b2a6882cf483b96
yaxyobekb <pubgn9642@gmail.com>
2024-09-09 00:22:39 +0500

    add admin keyboards config

---
+++ src/keyboards/admin-keyboards.js
@@ -0,0 +1,24 @@
+const adminKeyboards = {
+  parse_mode: "Markdown",
+  reply_markup: {
+    resize_keyboard: true,
+    one_time_keyboard: false,
+    keyboard: [[{ text: "Postlar 📮", }, { text: "Statistika 📊" },],],
+  },
+};
+
+const controlPostsInlineKeyboards = {
+  parse_mode: "Markdown",
+  reply_markup: {
+    remove_keyboard: true,
+    resize_keyboard: true,
+    one_time_keyboard: false,
+    inline_keyboard: [
+      [{ text: "Postni qidirish", callback_data: "search_post" }],
+      [{ text: "Yangi post qo'shish", callback_data: "add_post" }],
+      [{ text: "Postni tahrirlash", callback_data: "edit_post" }],
+    ],
+  },
+};
+
+module.exports = { adminKeyboards, controlPostsInlineKeyboards };
